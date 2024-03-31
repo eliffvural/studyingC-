998bb35b63e066c183c2c92c4531b84abd29d6a6
Elif Vural <elifvural019@gmail.com>
2024-03-31 14:35:11 +0300

    commit for pointers

--- pointer.cpp
+++ pointer.cpp
@@ -11,6 +11,26 @@ int main(){
     *ptr=8;
         std::cout<<t[0]<<" "<<t[1]<<" "<<t[2]<<std::endl;
 
+    (*ptr)++;
+            std::cout<<t[0]<<" "<<t[1]<<" "<<t[2]<<std::endl;
+
+    *ptr++=5;
+                std::cout<<t[0]<<" "<<t[1]<<" "<<t[2]<<std::endl;
+
+    *ptr=6;
+                    std::cout<<t[0]<<" "<<t[1]<<" "<<t[2]<<std::endl;
+
+    (*++ptr)++;
+                        std::cout<<t[0]<<" "<<t[1]<<" "<<t[2]<<std::endl;
+
+
+
+
+
+
+
+
+
 
 
 }
